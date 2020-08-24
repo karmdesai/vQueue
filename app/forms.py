d1e70550b5a8f19cadf7f6d1d9de09c08ee9c980
Karm Desai <karmdesai18@gmail.com>
2020-08-24 14:02:55 -0400

    update login/register fields

--- app/forms.py
+++ app/forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, IntegerField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, SelectField, EmailField
 from wtforms.validators import DataRequired, NumberRange, Length
 from app import client
 from app.helper import getAvailableNumbers
 
 class LoginForm(FlaskForm):
-    uName = StringField('Your Email', validators=[DataRequired()])
+    uName = EmailField('Your Email', validators=[DataRequired()])
     uPassword = PasswordField('Your Password', validators=[DataRequired()])
     
     submitNow = SubmitField('Login')
 
 class RegisterForm(FlaskForm):
-    uName = StringField("Your Email", validators=[DataRequired(), Length(min=3, max=24)])
+    uName = EmailField("Your Email", validators=[DataRequired(), Length(min=3, max=24)])
     uPassword = PasswordField('Your Password', validators=[DataRequired(), Length(min=8, max=20)])
     bName = StringField("Your Business's Name", validators=[DataRequired(), Length(min=3, max=24)])
 
