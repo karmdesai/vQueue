efb8a3189ff9158a908678e043f7da01728f72ef
Karm Desai <karmdesai18@gmail.com>
2020-09-13 12:32:24 -0400

    add bulma

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from config import Config
 from twilio.rest import Client
 from flask_pymongo import PyMongo
-from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 
 app = Flask(__name__)
@@ -13,6 +12,5 @@ mail = Mail(app)
 client = Client(app.config['TWILIO_ACCOUNT_SID'], 
     app.config['TWILIO_AUTH_TOKEN'])
 mongo = PyMongo(app)
-bootstrap = Bootstrap(app)
 
 from app import routes
