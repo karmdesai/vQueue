aad66eab1d76da852bdc1e7380a2a2c845f2005c
Karm Desai <karmdesai18@gmail.com>
2020-12-03 00:15:24 -0500

    change styling

--- app/forms.py
+++ app/forms.py
@@ -5,37 +5,41 @@ from wtforms.validators import DataRequired, NumberRange, Length, Email
 from app import client
 from app.helper import getAvailableNumbers
 
+standardInput = {'class': 'input is-size-5'}
+smallInput = {'class': 'input is-size-6'}
+submitInput = {'class': 'button is-primary is-size-5'}
+
 class LoginForm(FlaskForm):
-    uName = EmailField('Your Email', validators=[DataRequired(), Email()])
-    uPassword = PasswordField('Your Password', validators=[DataRequired()])
+    uName = EmailField('Your Email', validators=[DataRequired(), Email()], render_kw=standardInput)
+    uPassword = PasswordField('Your Password', validators=[DataRequired()], render_kw=standardInput)
     
-    submitNow = SubmitField('Login')
+    submitNow = SubmitField('Login', render_kw=submitInput)
 
 class RegisterForm(FlaskForm):
-    uName = EmailField("Your Email", validators=[DataRequired(), Email()])
-    uPassword = PasswordField('Your Password', validators=[DataRequired(), Length(min=8, max=24)])
-    bName = StringField("Your Business's Name", validators=[DataRequired(), Length(min=3, max=24)])
+    uName = EmailField("Your Email", validators=[DataRequired(), Email()], render_kw=smallInput)
+    uPassword = PasswordField('Your Password', validators=[DataRequired(), Length(min=8, max=24)], render_kw=smallInput)
+    bName = StringField("Your Business's Name", validators=[DataRequired(), Length(min=3, max=24)], render_kw=smallInput)
 
     uPhone = SelectField('Choose a Phone Number', choices=getAvailableNumbers(client, "CA"), validators=[DataRequired()])
 
-    submitNow = SubmitField('Sign Up')
+    submitNow = SubmitField('Sign Up', render_kw=submitInput)
 
 class CreateRoomForm(FlaskForm):
-    rMax = IntegerField("Your Store's Max Capacity", validators=[DataRequired(), NumberRange(min=1)])
-    rCustomers = IntegerField('# of Customers Already Inside', validators=[DataRequired()])
+    rMax = IntegerField("Your Store's Max Capacity", validators=[DataRequired(), NumberRange(min=1)], render_kw=standardInput)
+    rCustomers = IntegerField('# of Customers Already Inside', validators=[DataRequired(), NumberRange(min=0)], render_kw=standardInput)
 
-    submitNow = SubmitField('Create Queue')
+    submitNow = SubmitField('Create Queue', render_kw=submitInput)
 
 class ManualAddForm(FlaskForm):
-    cID = StringField("Person's Name", validators=[DataRequired()])
-    groupSize = IntegerField("# of People", validators=[DataRequired(), NumberRange(1, 50)])
+    cID = StringField("Person's Name", validators=[DataRequired()], render_kw=standardInput)
+    groupSize = IntegerField("# of People", validators=[DataRequired(), NumberRange(1, 50)], render_kw=standardInput)
 
-    manualAdd = SubmitField('Add User')
+    manualAdd = SubmitField('Add User', render_kw=submitInput)
 
 class ResetPasswordRequestForm(FlaskForm):
-    uEmail = EmailField('Your Email', validators=[DataRequired(), Email()])
-    submitNow = SubmitField('Request Password Reset')
+    uEmail = EmailField('Your Email', validators=[DataRequired(), Email()], render_kw=standardInput)
+    submitNow = SubmitField('Request Password Reset', render_kw=submitInput)
 
 class ResetPasswordForm(FlaskForm):
-    uPass = PasswordField('Your New Password', validators=[DataRequired(), Length(min=8, max=24)])
-    submitNow = SubmitField('Reset Password')
+    uPass = PasswordField('Your New Password', validators=[DataRequired(), Length(min=8, max=24)], render_kw=standardInput)
+    submitNow = SubmitField('Reset Password', render_kw=submitInput)
